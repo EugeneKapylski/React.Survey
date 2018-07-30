83f303aa4a50f8da6ad093a539994877431a5430
Evgeny Kapylsky <kapaminski@tut.by>
2018-07-30 14:42:36 +0300

    Added a project with unit tests.

---
+++ Survey.Business.XUnit.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Survey.Business.XUnit.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal(5, 6);
+        }
+    }
+}
