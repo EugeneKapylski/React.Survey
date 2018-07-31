0a4edab31f4b659c308ac82b283d2f6208bcca86
Evgeny Kapylsky <kapaminski@tut.by>
2018-07-31 13:55:27 +0300

    test.

---
+++ Survey.Business.XUnit.Test2/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Survey.Business.XUnit.Test2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal(5, 8);
+        }
+    }
+}
